79ef40f331b77db649b3ff98549c1943ba28ae9a
blackcoffeexbt <87530449+blackcoffeexbt@users.noreply.github.com>
2023-07-19 14:06:34 +0100

    Added helpers.h

---
+++ Lora32Sender/src/helpers.h
@@ -0,0 +1,23 @@
+#pragma once
+
+
+/**
+ * @brief Display text on the OLED
+ * 
+ * @param message 
+ * @param x pos - optional
+ * @param y pos - optional
+ */
+void oledDisplay(String message, int x = 5, int y = 20, bool clearScreen = true) {
+#ifdef HAS_DISPLAY
+    if (u8g2) {
+        char buf[256];
+        if (clearScreen) {
+            u8g2->clearBuffer();
+        }
+        snprintf(buf, sizeof(buf), message.c_str());
+        u8g2->drawStr(x, y, buf);
+        u8g2->sendBuffer();
+    }
+#endif
+}
